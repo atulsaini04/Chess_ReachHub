62a64a922d7f856f324d7ed3ee2bba409d96ec15
Atul Saini <satulnet2@gmail.com>
2023-11-14 02:20:02 +0530

    Delete fastapi_project/database.py

--- fastapi_project/database.py
+++
@@ -1,9 +0,0 @@
-# database.py
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://sde:hello@localhost/reachhubnew"
-engine = create_engine(DATABASE_URL, pool_size=5, max_overflow=10)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
